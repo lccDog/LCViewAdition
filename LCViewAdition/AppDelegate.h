3277fec9e9d3f589d3ff1dade7c5c94323e66491
lccDog <1046803106@qq.com>
2018-11-01 14:48:58 +0800

    提交

---
+++ LCViewAdition/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LCViewAdition
+//
+//  Created by lcc on 2018/10/30.
+//  Copyright © 2018年 lcc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
